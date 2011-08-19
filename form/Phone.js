2f6830133f784a4d1a0422bc924e6e3eb8b99abc
mdmadph <mdmadph@gmail.com>
2011-08-19 08:09:18 -0500

    Updated Phone.js

--- form/Phone.js
+++ form/Phone.js
@@ -17,7 +17,7 @@ Ext.define('Ext.ux.form.Phone', {
 		if (!Ext.form.field.VTypes.phone) {
 			Ext.apply(Ext.form.field.VTypes, {
 				phone: function (val) {
-					return (/^\d{3}\-\d{3}\-\d{4}$/).test(val);
+					return (/^\d{3}(\-)?\d{3}(\-)?\d{4}$/).test(val);
 				},
 				phoneText: this.self.validPhoneRegexMsg,
 				phoneMask: this.self.PhoneMask
@@ -27,9 +27,27 @@ Ext.define('Ext.ux.form.Phone', {
 		Ext.apply(this, {
 			vtype: 'phone',
 			maxLength: 12,
-			enforceMaxLength: true
+			enforceMaxLength: true, 
+			listeners: {
+				validitychange: this.onValidChange
+			}
 		});
 		
 		this.callParent(arguments);
+	},
+	
+	onValidChange: function(field, isValid) {
+		var _value = field.getValue(),
+		_phoneRegex = /^(\d{3})(\d{3})(\d{4})$/,
+		_result = null;
+		
+		if (_value.length == 10) {
+			_result = _phoneRegex.exec(_value);
+			if (_result) {
+				field.setValue(
+					_result[1] + "-" + _result[2] + "-" + _result[3]
+				);
+			}
+		}
 	}
 });
